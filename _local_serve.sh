58cb4981221626eeb359cde73c1c3f0acd0c583f
Arturo Borrero Gonzalez <arturo.bg@arturo.bg>
2025-04-17 12:30:54 +0200

    _posts/: add 2025-04-17-lts.md
    Add new post.
    
    Signed-off-by: Arturo Borrero Gonzalez <arturo.bg@arturo.bg>

--- _local_serve.sh
+++ _local_serve.sh
@@ -7,4 +7,4 @@ echo 'source "https://rubygems.org"' >> Gemfile
 echo 'gem "jekyll"' >> Gemfile
 echo 'gem "jekyll-paginate"' >> Gemfile
 
-docker run --rm --volume="$PWD:/site" -p 4000:4000 bretfisher/jekyll-serve
+podman run --rm --volume="$PWD:/site" -p 4000:4000 docker.io/bretfisher/jekyll-serve
